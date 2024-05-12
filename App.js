c01c8fb97dec906d40ca3640da35d859295172f7
vamsikeshwaran <vamsikeshwaran@gmail.com>
2024-05-12 13:34:32 +0530

    Final update is done

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Tabs from './navigation/Tabs';
 
-export default function App() {
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: 'black' }}>
+      {isLoading ? (
+        <LinearGradient colors={["#0c4cb4", "#c86ce4"]} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Image style={{ height: 400, width: 400 }} source={require('./assets/SOcioConnect.gif')} />
+        </LinearGradient>
+      ) : (
+        <NavigationContainer>
+          <View style={{ flex: 1 }}>
+            <Tabs />
+
+          </View>
+        </NavigationContainer>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
